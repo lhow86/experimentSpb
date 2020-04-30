98a4e27a2ef3918f8d1d85449d73ef960bab6e97
liuhao <liuhao@easipass.com>
2020-04-30 14:24:10 +0800

    apollo、sentinel

---
+++ src/main/java/com/experimentSpb/business/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.experimentSpb.business.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/")
+public class TestController {
+
+    private static Log log = LogFactory.getLog(TestController.class);
+
+    @Value("${settings.test}")
+    public String test;
+
+    @GetMapping("/hot")
+//    @SentinelResource("hot")
+    public String get() {
+        return "success";
+    }
+
+}
