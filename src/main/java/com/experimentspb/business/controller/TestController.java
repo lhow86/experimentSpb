8dd9b96be078b890a9b120ee9d4a7c456a25aeb6
lhow <130288669@qq.com>
2020-05-05 12:53:52 +0800

    启动脚本 | 打包task | 外部配置文件 | logback日志配置文件

--- src/main/java/com/experimentspb/business/controller/TestController.java
+++ src/main/java/com/experimentspb/business/controller/TestController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api")
+@RestController
+@RequestMapping("api/")
 public class TestController {
 
     private static Log log = LogFactory.getLog(TestController.class);
