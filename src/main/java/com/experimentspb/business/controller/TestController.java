303c3ef9ee6fcfb53e4e12f4167dd5b3c952bb77
liuhao <liuhao@easipass.com>
2020-05-06 12:41:03 +0800

    gradle doLast

--- src/main/java/com/experimentspb/business/controller/TestController.java
+++ src/main/java/com/experimentspb/business/controller/TestController.java
@@ -1,11 +1,8 @@
 package com.experimentspb.business.controller;
 
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
