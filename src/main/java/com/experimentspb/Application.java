0b6a39bbf26d5dd07feb4828d507a30d9b95571d
liuhao <liuhao@easipass.com>
2020-05-09 10:42:35 +0800

    Sentinel-Apollo experi

--- src/main/java/com/experimentspb/Application.java
+++ src/main/java/com/experimentspb/Application.java
@@ -1,10 +1,11 @@
 package com.experimentspb;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableApolloConfig
+@EnableApolloConfig
 //@EnableDiscoveryClient
 public class Application {
 
